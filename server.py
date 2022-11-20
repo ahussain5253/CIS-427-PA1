d467100e2def8589616a70d960080d4d45159e62
Ammar Hussain <ammarh786@gmail.com>
2022-11-20 00:35:12 -0500

    a

--- server.py
+++ server.py
@@ -63,7 +63,6 @@ def addFunds(amt,userID):
     print("\n\nFunds added successfully\nNew Balance: $", currAmt)
     print("\n")
 
-
 def buy(command, cryptoName, cryptoAmt, pricePerCrypto, userID):
     
     for row in u.execute("SELECT count(*) FROM Users WHERE ID = ?", userID):
@@ -71,7 +70,7 @@ def buy(command, cryptoName, cryptoAmt, pricePerCrypto, userID):
 
     #Checks if user id exists or not then prompts to make a new one
     if (uidexists == 0):
-        createUID = input("\n\n400 message format error\nUser ID not found\nMake new user? Type Y or N\n\n")
+        createUID = input("\n\n400 user not found error\nUser ID not found\nMake new user? Type Y or N\n\n")
         
         if (createUID == 'y') or (createUID == 'Y'):
             create()
@@ -136,6 +135,77 @@ def buy(command, cryptoName, cryptoAmt, pricePerCrypto, userID):
                         print("BOUGHT: New balance: " + newBal + " " + cryptoName + ". " + "USD balance $%.2f" % currAmt + "\n")
 
                         break  
+   
+def sell(command, cryptoName, cryptoAmt, pricePerCrypto, userID):
+    
+    #Check if there is a user id record in the user table
+    for row in u.execute("SELECT count(*) FROM Users WHERE ID = ?", userID):
+        uidexistsUs = row[0]
+    
+    for row in u.execute("SELECT count(*) FROM Cryptos WHERE user_id = ?", userID):
+        uidexistsCr = row[0]
+            
+    #Checks if user id exists or not then prompts to make a new one
+    if (uidexistsUs == 0) :
+        createUID = input("\n\n400 user not found error\nUser ID not found in Users\nMake new user? Type Y or N\n\n")
+        
+        if (createUID == 'y') or (createUID == 'Y'):
+            create()
+    else:   
+        if (uidexistsCr == 0):
+            buyCrypto = input("\n\n400 no crypto record for user error\nUser specified has never bought crypto and has no record\nWould you like to buy crypto? Type Y or N\n\n")
+            
+            if (buyCrypto == 'Y') or (buyCrypto == 'y'):
+                cryptoName = input("\n\nType in the name of the crypto to buy\n\n")
+                cryptoAmt = input("\n\nType in the amount of crypto to buy\n\n")
+                pricePerCrypto = input("\n\nType in the price per each crypto\n\n")
+                
+                buy("BUY", cryptoName, cryptoAmt, pricePerCrypto, userID)
+        else:
+            for row in u.execute("SELECT count(*) FROM Cryptos WHERE crypto_name = ? AND user_id = ?", (cryptoName, userID)):
+                crypuidExists = row[0]
+            
+            if (crypuidExists == 0):
+                bCrypto = input("\n\n300 no crypto record found for user error\nCrypto specified has never been bought by user and has no record\nWould you like to buy some " + cryptoName + " crypto? Type Y or N\n\n")
+                
+                if (bCrypto == 'y') or (bCrypto == 'Y'):
+                    cryptoAmt = input("\n\nType in the amount of "+ cryptoName +" to buy\n\n")
+                    pricePerCrypto = input("\n\nType in the price per each "+ cryptoName +"\n\n")
+                    
+                    buy("BUY", cryptoName, cryptoAmt, pricePerCrypto, userID)
+                
+                else:         
+                    print('\nRecieved: ' + command + '\n' + '200 OK')
+    
+                    amt = float(cryptoAmt)
+                    ppc = float(pricePerCrypto)
+    
+                    price = amt * ppc
+            
+                    for row in u.execute("SELECT usd_balance FROM Users WHERE ID = ?", (userID)):
+                        currAmt = row[0]
+                        break
+        
+                    currAmt += price
+    
+                    u.execute("UPDATE Users SET usd_balance = ? WHERE ID = ?", (currAmt, userID))
+                    conn.commit()
+    
+                    for row in u.execute("SELECT crypto_balance FROM Cryptos WHERE user_id = ? AND crypto_name = ?", (userID, cryptoName)):
+                        cryptBal = row[0]
+                
+                        nAmt = float(cryptoAmt)
+                        cryptBal -= nAmt
+                
+                        u.execute("UPDATE Cryptos SET crypto_balance = ? WHERE user_id = ? AND crypto_name = ?", (cryptBal, userID, cryptoName))
+                        conn.commit()
+                
+                        newBal = str(cryptBal)
+    
+                        print("SOLD: New balance: " + newBal + " " + cryptoName + ". " + "USD balance $%.2f" % currAmt + "\n")
+
+                        break 
+       
        
 def shutdown():
     exit()
@@ -147,7 +217,7 @@ while True:
     splitcommand = command.split()
     
     if (splitcommand[0] == 'CREATE'):
-               
+                     
         create()
         
     if (splitcommand[0] == 'BUY'):
@@ -158,6 +228,15 @@ while True:
         uid = splitcommand[4]
         
         buy(command, name, amt, ppc, uid)
+           
+    if (splitcommand[0] =='SELL'):
+           
+        name = splitcommand[1]
+        amt = splitcommand[2]
+        ppc = splitcommand[3]
+        uid = splitcommand[4]
+        
+        sell(command, name, amt, ppc, uid)    
                 
     if (splitcommand[0] == 'CUSTOM'):
         
